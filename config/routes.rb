9e82245c8d9160257371e1c1a5fb47c920fd61e7
laoooba <yama10taku31@gmail.com>
2021-07-15 07:36:36 +0000

    [add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  get '/home/about', to: 'homes#show', as: 'about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   resources :books, only:[:new, :create, :index, :show, :edit, :destroy]
-  
+
   resources :users, only:[:show, :edit, :update]
 end
