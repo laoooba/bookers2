04c9e05a0224830e5b77ec69aa29edb217da134e
laoooba <yama10taku31@gmail.com>
2021-07-17 09:49:52 +0000

    [add]book show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,30 +1,35 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to books_path
+      redirect_to book_path(@book)
     else
       render :new
     end
   end
 
   def index
+    @book = Book.new
     @books = Book.all
     @user = current_user
   end
 
   def show
+    @book_new = Book.new
     @book = Book.find(params[:id])
     @user = @book.user
   end
 
   def edit
     @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book)
   end
 
   def destroy
@@ -36,7 +41,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.permit(:title, :body, :user_id)
+    params.require(:book).permit(:title, :body, :user_id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get '/home/about', to: 'homes#show', as: 'about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :books, only:[:new, :create, :index, :show, :edit, :destroy]
+  resources :books, only:[:create, :index, :show, :edit, :destroy, :update]
 
   resources :users, only:[:show, :edit, :update]
 end
