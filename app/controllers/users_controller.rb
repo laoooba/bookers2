61bf54a0658bebb975b29f6610973cfacea0bcdd
laoooba <yama10taku31@gmail.com>
2021-07-17 14:10:10 +0000

    [add]user update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,32 @@
 class UsersController < ApplicationController
+
+  def index
+    @book = Book.new
+    @users = User.all
+    @user = current_user
+  end
+
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
-  end 
-  
+  end
+
   def edit
     @user = User.find(params[:id])
-  end 
-  
+  end
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
   end
-  
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
-  
-  
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
 
   resources :books, only:[:create, :index, :show, :edit, :destroy, :update]
 
-  resources :users, only:[:show, :edit, :update]
+  resources :users, only:[:index, :show, :edit, :update]
 end
