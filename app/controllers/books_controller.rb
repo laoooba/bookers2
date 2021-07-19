872253768f891ada866b32f539062f9ec1d19f0e
laoooba <yama10taku31@gmail.com>
2021-07-19 09:56:32 +0000

    [finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :correct_book,only: [:edit]
 
   def create
     @book = Book.new(book_params)
@@ -51,5 +53,12 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body, :user_id)
   end
+
+  def correct_book
+        @book = Book.find(params[:id])
+    unless @book.user.id == current_user.id
+      redirect_to books_path
+    end
+end
 
 end
