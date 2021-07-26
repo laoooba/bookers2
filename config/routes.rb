5a81a34e681d7e8c8b5dffe46477e8f2a113e883
laoooba <yama10taku31@gmail.com>
2021-07-26 09:48:15 +0000

    [finish]

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :books, only:[:create, :index, :show, :edit, :destroy, :update]do
-    resource :favorites, only:[:create, :destroy]
+    resources :book_comments, only:[:create, :destroy]
+    resources :favorites, only:[:create, :destroy]
   end
 
   resources :users, only:[:index, :show, :edit, :update]
