f012536af3ac77bbdb6d57af9b9579029c02cc71
laoooba <yama10taku31@gmail.com>
2021-08-07 07:47:46 +0000

    [finish]7a

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :favorited_books, through: :favorites, source: :book
+  
 
   has_many :active_relationships, class_name:  "Relationship", foreign_key: "follower_id", dependent:   :destroy
   has_many :passive_relationships, class_name:  "Relationship", foreign_key: "followed_id", dependent:   :destroy
